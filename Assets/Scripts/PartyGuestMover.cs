726edb74f6d6e643699ff1a53f8302c5905c63b4
Mab497 <willi.buecking@students.hs-mainz.de>
2022-01-27 14:10:14 +0100

    make moving party-guests

--- Assets/Scripts/PartyGuestMover.cs
+++ Assets/Scripts/PartyGuestMover.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PartyGuestMover : MonoBehaviour
 {
+
+    private int speed;
+    public int lowerSpeedBound;
+    public int upperSpeedBound;
     // Start is called before the first frame update
     void Start()
     {
+        speed = Random.Range(lowerSpeedBound, upperSpeedBound);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = Quaternion.AngleAxis((Mathf.PingPong(Time.time*speed, 20f)-10), Vector3.left);
     }
 }
